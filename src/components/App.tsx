dfe642dbc5061e9c275efb70d21c844a67d0cad8
AtakhanovRodion <atakhanovrodion@gmail.com>
2021-05-02 14:41:31 +0500

    init commit

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+  const value: number = 3;
+  return <div> tssst {value}</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js)/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
+};
